cbd4af71e5112b968b836a4c1e96052acf31cec5
Leonardo Sainz <leo.database92@gmail.com>
2023-01-17 20:49:04 -0300

    feat: agrego page react query

---
+++ pages/project/react-query.tsx
@@ -0,0 +1,19 @@
+import { Box, Typography } from "@mui/material";
+
+import { PortfolioLayout } from "@/components/layout";
+import React from "react";
+
+const ReactQuery = () => {
+  return (
+    <>
+    <PortfolioLayout
+        title={"Portfolio"}
+        pageDescription={"Portafolio descripcion"}
+      >
+        react
+      </PortfolioLayout>
+    </>
+  );
+};
+
+export default ReactQuery;
